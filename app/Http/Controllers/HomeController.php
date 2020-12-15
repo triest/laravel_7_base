8f1a7d259286e3ece6d564309c960106094c908e
triest <you@example.com>
2020-12-15 23:16:01 +0300

    Добавление загрузки файлов

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class HomeController extends Controller
 {
@@ -13,7 +14,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+    //    $this->middleware('auth');
     }
 
     /**
@@ -24,5 +25,14 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+    }
+
+    public function upload(Request $request)
+    {
+        $path = $request->file('file')->store('public/avatars');
+        $url = Storage::url($path);
+        dump($url);
+
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +21,13 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::prefix('upload')->name('upload.')->group(function () {
+    Route::get('/', function () {
+        // Route assigned name "admin.users"...
+        return view('upload');
+    })->name('main');
+
+    Route::post('/upload',[HomeController::class,'upload'])->name('uploadFile');
+
+});
